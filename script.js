3c95ffcb90a5c08714c3bc595a4464e32c461941
sultan288 <sultan.ist28@gmail.com>
2022-01-06 02:23:50 +0600

    updated Travel agency

--- script.js
+++ script.js
@@ -41,3 +41,48 @@ vidBtn.forEach(btn =>{
         document.querySelector('#video-slider').src = btnSrc;
     })
 })
+
+// review slider
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+        slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+}
+});
+
+// brand slider
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        450: {
+            slidesPerView: 2
+        },
+        768: {
+        slidesPerView: 3,
+        },
+        991: {
+            slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+        }
+}
+});
